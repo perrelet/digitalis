d5dd148696ea2acad0e81d36307258d4ce1a3184
Jamie <jperrelet@gmail.com>
2025-08-02 16:45:48 +0000

    fix: `ACF_Names` ACF 2.94 `$label = esc_html( $field['label'] )`

--- modules/acf_names/acf_names.php
+++ modules/acf_names/acf_names.php
@@ -6,15 +6,13 @@ class ACF_Names {
 	
 	function __construct () {
 		
-		if (class_exists('acf') && is_admin()) add_action('acf/prepare_field', [$this, 'prepare_field']);
+		if (class_exists('acf') && is_admin()) add_filter('acf/get_field_label', [$this, 'get_field_label'], 10, 3);
 		
 	}
 	
-	public function prepare_field( $field ) {
+	public function get_field_label ($label, $field, $context) {
 		
 		if (current_user_can(DIGITALIS_ADMIN_CAP)) {
-			
-			$wrap = ["<span class='digit-acf-name'>", "</span>"];
 
 			if (array_key_exists("wpml_cf_preferences", $field)) {
 				
@@ -24,20 +22,79 @@ class ACF_Names {
 					WPML_COPY_ONCE_CUSTOM_FIELD => __("Copy once", 'acfml'),
 					WPML_TRANSLATE_CUSTOM_FIELD => __("Translate", "acfml")
 				);
-				
-				$html .= $wrap[0] . " - " . $wpml_choices[$field['wpml_cf_preferences']] . $wrap[1];
+
+				$name = $wpml_choices[$field['wpml_cf_preferences']];
 				
 			} else {
 
-				$html = $wrap[0] . " - " . $field["_name"] . $wrap[1];
+				$name = $field["_name"];
 
 			}
+
+			$label = "<div class='digit-acf-label'>{$label}</div><div class='digit-acf-name'><span>name: </span><span>{$name}</span></div>";
+
+			static $index;
+			if (!$index) {
+				$index = 0;
+				?>
+				<script>
+					document.addEventListener('DOMContentLoaded', function () {
+						document.querySelectorAll('.digit-acf-name > span:last-child').forEach(function (el) {
+							el.addEventListener('click', () => {
+								navigator.clipboard.writeText(el.innerText);
+								el.parentElement.classList.add(`data-copied`);
+								setTimeout(() => el.parentElement.classList.remove(`data-copied`), 1000);
+							});
+						});
+					});
+				</script>
+				<style>
+					@keyframes digit-acf-name {
+						0%   { opacity: 0.25; }
+						100% { opacity: 1; }
+					}
+					.acf-label.acf-label > label {
+						display: flex;
+						flex-direction: row;
+						justify-content: space-between;
+					}
+					.digit-acf-name {
+						> span {
+							opacity: 0.25;
+						}
+						> span:last-child {
+							cursor: pointer;
+							display: inline-block;
+							&:hover {
+								opacity: 1;
+							}
+							&.data-copied,
+							&:active {
+								
+								opacity: 1;
+							}
+						}
+						&.data-copied {
+							
+							> span:last-child {
+								opacity: 1;
+								color: #0ac100;
+								animation: digit-acf-name 0.75s ease-in-out infinite;
+								
+							}
+						}
+					}
+				</style>
+				<?php
+			}
 			
-			$field['label'] .= $html;
+			$index++;
+			
+			//$field['label'] .= $html;
 			
 		}
 
-		return $field;
+		return $label;
 	}
 	
 }
